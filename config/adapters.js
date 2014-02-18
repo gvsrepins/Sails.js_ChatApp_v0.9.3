2c187c25b2ebd9788d1ee13baf06c92f889e2059
francisco pimentel <francisco.bcpf@gmail.com>
2014-02-18 00:28:45 -0300

    fix no node

--- config/adapters.js
+++ config/adapters.js
@@ -14,28 +14,17 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'disk',
-
-  // In-memory adapter for DEVELOPMENT ONLY
-  memory: {
-    module: 'sails-memory'
-  },
-
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data IS preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
+  'default': 'mysql',
 
   // MySQL is the world's most popular relational database.
   // Learn more: http://en.wikipedia.org/wiki/MySQL
   mysql: {
     module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
+    host: 'aulaaws.cwwszn67aibz.sa-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'filipe',
+    password: '12345678',
+    database: 'mydb'
   }
+ 
 };
