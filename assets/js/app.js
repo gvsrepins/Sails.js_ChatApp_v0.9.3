0e0fe5f02c9357aa89dd2bdd2d55485acb31c0ba
francisco pimentel <francisco.bcpf@gmail.com>
2014-02-18 01:06:16 -0300

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@
 (function (io) {
 
   // as soon as this file is loaded, connect automatically, 
-  var socket = io.connect('http://localhost:8080');
+  var socket = io.connect('http://54.207.138.52:80');
   if (typeof console !== 'undefined') {
     log('Connecting to Sails.js...');
   }
